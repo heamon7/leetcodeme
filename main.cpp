d70971adf71cf3f2435f7632106cc4d3acf71bc1
heamon7 <heamon7@gmail.com>
2015-10-08 14:05:39 +0800

    after execute nums.erase(it) in line 19 ,the size of the vector decreased,but the iterator it  not changed ,so the it overflowed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() <= 1){
+            return nums.size();
+        }
+        else{
+            int index = 0;
+            for(vector<int> ::iterator it=nums.begin()+1;it!=nums.end();++it){
+                if(nums.at(index)!=*it){
+                    nums.at(++index) = *it;
+                }
+//                else{
+//                    nums.erase(it);
+//                }
+            }
+            return index + 1;
+        }
+    }
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+    int a[] = {1,2};
+    vector<int> v(2,1);
+
+
+    cout<<v.size()<<endl;
+    Solution solution;
+    cout<<solution.removeDuplicates(v);
+
     return 0;
 }
