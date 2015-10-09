c97a04f6de48df1f74ccc99e89219eaf247a9a0d
heamon7 <heamon7@gmail.com>
2015-10-09 11:59:58 +0800

    ac 20ms: change ++pos to ++pos and add nums.size()?1:0 to solve nums is empty

--- main.cpp
+++ main.cpp
@@ -9,25 +9,24 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int flag = 0;
-        int pos = 0;
+        int pos = nums.size()?1:0;
         for(int i = 1;i < nums.size();i++){
             if(nums[i] == nums[i-1]){
                 if(flag == 0){
                     flag++;
-                    nums[++pos] = nums[i];
+                    nums[pos++] = nums[i];
                 }
             }
             else{
-
-                nums[++pos] = nums[i];
+                nums[pos++] = nums[i];
                 flag = 0;
             }
         }
-        nums.resize(pos+1);
-        return pos+1;
-
+        nums.resize(pos);
+        return pos;
     }
 };
+
 int main() {
     int a[] = {1,2,2,2,3,3,3,3,3,4,5,5,6,7,8,8,9,10};
     vector<int> v(a,a + sizeof(a)/sizeof(a[0]));
